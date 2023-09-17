11628db58ded1f9a89938354aa8357f0fff0dab5
khe <khj778878@naver.com>
2023-09-18 04:29:07 +0900

    [refact]: React.lazy적용(성능 최적화:코드 스플리팅을 통하여 번들링 파일을 나눔)

--- client/src/routes/MyRoute.tsx
+++ client/src/routes/MyRoute.tsx
@@ -1,14 +1,16 @@
+import React, { lazy, Suspense } from "react";
 import { Routes, Route, Outlet } from "react-router-dom";
 import PrivateRoute from "./PrivateRoute";
-import LoginMainPage from "../pages/LoginMainPage";
-import LoginInputPage from "../pages/LoginInputPage";
-import SignUpPage from "../pages/SignUpPage";
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
-import MainPage from "../pages/MainPage";
-import SearchPage from "../pages/SearchPage";
-import PlayMoviePage from "../pages/PlayMoviePage";
-import NotFoundPage from "../pages/NotFoundPage";
+
+const LoginInputPage = lazy(() => import("../pages/LoginInputPage"));
+const LoginMainPage = lazy(() => import("../pages/LoginMainPage"));
+const SignUpPage = lazy(() => import("../pages/SignUpPage"));
+const MainPage = lazy(() => import("../pages/MainPage"));
+const SearchPage = lazy(() => import("../pages/SearchPage"));
+const PlayMoviePage = lazy(() => import("../pages/PlayMoviePage"));
+const NotFoundPage = lazy(() => import("../pages/NotFoundPage"));
 
 const Layout = () => {
   return (
@@ -22,25 +24,27 @@ const Layout = () => {
 
 const MyRoute = () => {
   return (
-    <Routes>
-      {/* 인증을 반드시 하지 않아야만 접속 가능한 페이지 정의 */}
-      <Route element={<PrivateRoute authentication={false} />}>
-        <Route path="/blind" element={<LoginMainPage />} />
-        <Route path="/login" element={<LoginInputPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-      </Route>
-
-      {/* 인증을 반드시 해야지만 접속 가능한 페이지 정의 */}
-      <Route element={<PrivateRoute authentication={true} />}>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<MainPage />} />
-          <Route path="/search" element={<SearchPage />} />
-          <Route path="/playmovie/:movieId" element={<PlayMoviePage />} />
+    <Suspense fallback={null}>
+      <Routes>
+        {/* 인증을 반드시 하지 않아야만 접속 가능한 페이지 정의 */}
+        <Route element={<PrivateRoute authentication={false} />}>
+          <Route path="/blind" element={<LoginMainPage />} />
+          <Route path="/login" element={<LoginInputPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
         </Route>
-      </Route>
 
-      <Route path="/*" element={<NotFoundPage />} />
-    </Routes>
+        {/* 인증을 반드시 해야지만 접속 가능한 페이지 정의 */}
+        <Route element={<PrivateRoute authentication={true} />}>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<MainPage />} />
+            <Route path="/search" element={<SearchPage />} />
+            <Route path="/playmovie/:movieId" element={<PlayMoviePage />} />
+          </Route>
+        </Route>
+
+        <Route path="/*" element={<NotFoundPage />} />
+      </Routes>
+    </Suspense>
   );
 };
 export default MyRoute;
